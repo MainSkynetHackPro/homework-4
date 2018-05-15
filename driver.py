a96defb3b8c5f981336817eadddab6685708b973
Nikolay <yaminsky96@gmail.com>
2018-05-15 23:57:56 +0300

    added support for geckodriver

--- driver.py
+++ driver.py
@@ -1,5 +1,15 @@
 from selenium import webdriver
+import os
+
+CHROME_DRIVER = 'chrome'
+GECKO_DRIVER = 'firefox'
 
 
 def get_driver():
-    return webdriver.Chrome('./chromedriver')
+    driver = os.environ.get('DRIVER', 'chrome')
+    if driver == CHROME_DRIVER:
+        return webdriver.Chrome('./chromedriver')
+    elif driver == GECKO_DRIVER:
+        return webdriver.Firefox(executable_path='./geckodriver')
+    else:
+        raise Exception('Wrong driver specified, use "%s" or "%s"' % (CHROME_DRIVER, GECKO_DRIVER))
