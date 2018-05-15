21787afc3c0d9d88d7cdae4e7b90dabd7648b180
Nikolay <yaminsky96@gmail.com>
2018-05-15 23:16:44 +0300

    base files

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+from tests.test_kolya import TestKolya
+from tests.test_kostya import TestKostya
+from tests.test_max import TestMax
 
 if __name__ == '__main__':
-    pass
+    suite = unittest.TestSuite((
+        unittest.makeSuite(TestKolya),
+        unittest.makeSuite(TestKostya),
+        unittest.makeSuite(TestMax),
+    ))
+    result = unittest.TextTestRunner().run(suite)
---
+++ tests/test_kolya.py
@@ -0,0 +1,15 @@
+import unittest
+
+from driver import get_driver
+
+
+class TestKolya(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = get_driver()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_simple(self):
+        self.driver.get("http://park.mail.ru/")
